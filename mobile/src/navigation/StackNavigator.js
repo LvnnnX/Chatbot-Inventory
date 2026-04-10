81015936f358700cebf5ce1237b78ae4624964f9
LvnnnX <rickyhadiman@gmail.com>
2026-04-10 20:54:09 +0800

    fix: use native-stack instead of missing stack package

--- mobile/src/navigation/StackNavigator.js
+++ mobile/src/navigation/StackNavigator.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import ChatScreen from '../screens/ChatScreen';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
   return (
